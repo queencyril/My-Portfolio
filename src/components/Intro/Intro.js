db3a0a1f6a1ef118cfef49d1df083e15f3264b69
Oluchi Queen <osinachiiheagwara@gmail.com>
2024-01-29 10:52:33 +0100

    Random adjustments

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -16,7 +16,7 @@ const Intro = () => {
             Frontend Developer
           </span>
           <p className="introPara">
-            I a frontend developer with little experience in <br />
+            I am a frontend developer with experience in <br />
             creating visually appealing and user friendly interfaces.
           </p>
 
